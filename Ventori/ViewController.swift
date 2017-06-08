79dec28158fbbe756bf91db13de5a2bec5fe85ff
Yohannes Wijaya <readyforservice@icloud.com>
2017-06-08 12:24:24 +0700

    Created base project

---
+++ Ventori/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ventori
+//
+//  Created by Yohannes Wijaya on 6/8/17.
+//  Copyright © 2017 Corruption of Conformity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
