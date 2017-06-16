6d58a096103c4cf667155d979837792eaad46345
Yohannes Wijaya <readyforservice@icloud.com>
2017-06-16 18:02:52 +0700

    Added border & corner radius to text view and image view as well as ability to dismiss keyboard

--- Ventori/AddViewController.swift
+++ Ventori/AddViewController.swift
@@ -22,6 +22,7 @@ class AddViewController: UIViewController {
     
     @IBOutlet var inventoryImageView: UIImageView!
     @IBOutlet weak var inventoryNameTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet var minusButtonImageView: UIImageView!
     @IBOutlet var plusButtonImageView: UIImageView!
@@ -44,20 +45,39 @@ class AddViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.navigationItem.hidesBackButton = true
+        
         self.inventoryImageView.contentMode = .center
+        self.inventoryImageView.layer.borderWidth = 0.5
+        self.inventoryImageView.layer.borderColor = UIColor.lightGray.withAlphaComponent(0.50).cgColor
+        self.inventoryImageView.layer.cornerRadius = 5
         self.inventoryImageView.image = UIImage(named: self.boxIcon)
+        
         self.inventoryNameTextField.text = "Inventory Name"
+        
+        self.descriptionTextView.layer.borderWidth = 0.5
+        self.descriptionTextView.layer.borderColor = UIColor.lightGray.withAlphaComponent(0.50).cgColor
+        self.descriptionTextView.layer.cornerRadius = 5
+        
         self.counterLabel.text = "1"
         self.minusButtonImageView.image = UIImage(named: self.minusIcon)
         self.plusButtonImageView.image = UIImage(named: self.plusIcon)
         
-        self.navigationItem.hidesBackButton = true
+        let dismissKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(AddViewController.dismissKeyboardIfPresent))
+        self.view.addGestureRecognizer(dismissKeyboardGesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         self.navigationController?.isToolbarHidden = true
+    }
+    
+    // MARK: - Helper Methods
+    
+    func dismissKeyboardIfPresent() {
+        self.inventoryNameTextField.resignFirstResponder()
+        self.descriptionTextView.resignFirstResponder()
     }
 
     /*
