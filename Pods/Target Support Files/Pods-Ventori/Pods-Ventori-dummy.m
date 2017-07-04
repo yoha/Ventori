23dd978d9381c3eb44eaded83acbad19df0bf7a4
Yohannes Wijaya <readyforservice@icloud.com>
2017-07-04 14:31:49 +0700

    Integrated Firebase Cocoapods modules and Set it up for first usage.

---
+++ Pods/Target Support Files/Pods-Ventori/Pods-Ventori-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ventori : NSObject
+@end
+@implementation PodsDummy_Pods_Ventori
+@end
--- Ventori/AppDelegate.swift
+++ Ventori/AppDelegate.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    override init() {
+        super.init()
+        
+        FirebaseApp.configure()
+        Database.database().isPersistenceEnabled = true
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
